24d24b62e03920c5c298e53415f75ec92a0ac7a0
Matthias <matreff@vt.edu>
2020-02-04 12:47:57 +0100

    created PixelList and bug fixing with 12 o'clock hourCat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from datetime import datetime
 import random
+import itertools
 #import board
 #import neopixel
 
@@ -12,10 +13,14 @@ class WordClock:
     def returnHourCat(self):
         self.__hour = datetime.now().hour
         self.__min = datetime.now().minute
-        if self.__hour >=12:
+        if (self.__hour == 12 and self.__min>=25):
+            self.__hourCat = 1
+        elif self.__hour > 12:
             self.__hourCat = self.__hour - 12
-        if self.__min >= 25:
+        elif self.__min >= 25:
             self.__hourCat = self.__hourCat + 1
+        else:
+            self.__hourCat = self.__hour
         return str(self.__hourCat)
 
     def returnMinuteCat(self):
@@ -26,17 +31,20 @@ class WordClock:
         self.__minuteCategory = self.__minuteCategory.split("[")[1].split("]")[0]
         return self.__minuteCategory
 
-    def searchWords(self, wordList = []):
+    def searchLedPixel(self, wordList):
         s = self.__letterMatrix
+        pinVector = []
         for item in wordList:
             row = 0
             for line in s:
                 if item in ''.join(line):
                     print(item + " ["+ str(row) + "]["+str(line.find(item)) + ":" + str(line.find(item)+len(item))+ "]")
-                    break
+                    for l in range (0,len(item)):
+                        pinLightUp = ((row)*13) + (line.find(item) + l)
+                        pinVector.append(pinLightUp)
                 else:
                     row += 1
-
+        return pinVector
 
 #Setup Dictionaries for different word groups
 dicIntro  =	{
@@ -68,7 +76,7 @@ dicMinute  =	{
   "11": "fünf vor"
 }
 dicHour  =	{
-  "1": "ein",
+  "1": "eins",
   "2": "zwei",
   "3": "drei",
   "4": "vier",
@@ -98,14 +106,13 @@ randItIs = str(random.randint(1,2))
 randIntro = str(random.randint(1,8))
 randEnd = str(random.randint(1,4))
 
-#Get locations of used words
-ins.searchWords(dicIntro.get(randIntro).split())
-ins.searchWords(dicItIs.get(randItIs).split())
-ins.searchWords(dicMinute.get(minCat).split())
-ins.searchWords(dicHour.get(hourCat).split())
-ins.searchWords(dicEnd.get(randEnd).split())
+#get Word List to Pass to function for finding postion of LED pixels
+wordListToPass = [dicIntro.get(randIntro).split(), dicItIs.get(randItIs).split(),  dicMinute.get(minCat).split(), dicHour.get(hourCat).split(), dicEnd.get(randEnd).split()]
+wordListToPass = itertools.chain.from_iterable(wordListToPass)
 
 
+#pins to light up
+print(ins.searchLedPixel(wordListToPass))
 
 
 #pixels = neopixel.NeoPixel(board.D18, 30)
